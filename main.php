09adaef8d3d4bbe67bfd94ac28364362d58a8ce0
TharunMV <37066939+TharunMV@users.noreply.github.com>
2018-04-07 12:09:09 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,27 +1,13 @@
 <?php
-
-// original article url
-// http://www.script-tutorials.com/how-to-easily-make-a-php-chat-application
-
-// set error reporting level
 if (version_compare(phpversion(), "5.3.0", ">=") == 1)
   error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 else
   error_reporting(E_ALL & ~E_NOTICE);
-
-
-require_once('inc/login.inc.php');
-require_once('inc/chat.inc.php');
-
-// initialization of login system and generation code
+require_once('login.inc.php');
+require_once('chat.inc.php');
 $oSimpleLoginSystem = new SimpleLoginSystem();
-
 $oSimpleChat = new SimpleChat();
-
-// draw login box
 echo $oSimpleLoginSystem->getLoginBox();
-
-// draw chat application
 $sChatResult = 'Need login before using';
 if ($_COOKIE['member_name'] && $_COOKIE['member_pass']) {
     if ($oSimpleLoginSystem->check_login($_COOKIE['member_name'], $_COOKIE['member_pass'])) {
@@ -29,5 +15,4 @@ if ($_COOKIE['member_name'] && $_COOKIE['member_pass']) {
     }
 }
 echo $sChatResult;
-
 ?>
